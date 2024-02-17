de7e1de2af13104c75df395f22aaf021ae48a973
chellysA <chellys.castillo@gmail.com>
2024-02-17 13:49:24 -0400

    feat: translate into spanish

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { FcGoogle } from "react-icons/fc";
 import InputField from "@/components/fields/InputField";
 import Checkbox from "@/components/checkbox";
 import FixedPlugin from "@/components/fixedPlugin/FixedPlugin";
@@ -15,24 +14,11 @@ const LogIn: FC<Props> = () => {
           {/* Sign in section */}
           <div className="m-12  mt-[10vh] w-full max-w-full flex-col items-center md:pl-4 lg:pl-0 xl:max-w-[420px]">
             <h4 className="mb-2.5 text-4xl font-bold text-navy-700 dark:text-white">
-              Sign In
+              Ingresa
             </h4>
             <p className="mb-9 ml-1 text-base text-gray-600">
-              Enter your email and password to sign in!
+              Ingresa tu email y contraseña para acceder!
             </p>
-            <div className="mb-6 flex h-[50px] w-full items-center justify-center gap-2 rounded-xl bg-lightPrimary hover:cursor-pointer dark:bg-navy-800">
-              <div className="rounded-full text-xl">
-                <FcGoogle />
-              </div>
-              <h5 className="text-sm font-medium text-navy-700 dark:text-white">
-                Sign In with Google
-              </h5>
-            </div>
-            <div className="mb-6 flex items-center  gap-3">
-              <div className="h-px w-full bg-gray-200 dark:bg-navy-700" />
-              <p className="text-base text-gray-600 dark:text-white"> or </p>
-              <div className="h-px w-full bg-gray-200 dark:bg-navy-700" />
-            </div>
 
             {/* Email */}
             <InputField
@@ -48,7 +34,7 @@ const LogIn: FC<Props> = () => {
             <InputField
               variant="auth"
               extra="mb-3"
-              label="Password*"
+              label="Contraseña*"
               placeholder="Min. 8 characters"
               id="password"
               type="password"
@@ -62,19 +48,19 @@ const LogIn: FC<Props> = () => {
                   htmlFor="remember-me"
                   className="ml-2 text-sm font-medium text-navy-700 dark:text-white"
                 >
-                  Keep me logged In
+                  Mantener sesion abierta
                 </label>
               </div>
               <a
                 className="text-sm font-medium text-brand-500 hover:text-brand-600 dark:text-white"
                 href=""
               >
-                Forgot Password?
+                Olvidaste tu contraseña?
               </a>
             </div>
 
             <button className="linear mt-2 w-full rounded-xl bg-brand-500 py-[12px] text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200">
-              Sign In
+              Ingresar
             </button>
 
             <div className="mt-4">
@@ -83,7 +69,7 @@ const LogIn: FC<Props> = () => {
               </span>
               <a
                 href=" "
-                className="ml-1 text-sm font-medium text-brand-500 hover:text-brand-600 dark:text-white"
+                className="ml-1 text-sm font-medium text-brand-500 hover:text-brand-600 dark:text-brand-400"
               >
                 Create an account
               </a>
