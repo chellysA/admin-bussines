e02f9407d5f2f7d4f7bfa615b91d06ec9f7c0bd4
chellysA <chellys.castillo@gmail.com>
2024-02-23 10:58:19 -0400

    feat: center select arrow and add title to button

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,15 +1,24 @@
-import { useCallback, useEffect, useMemo } from "react";
+import { useMemo } from "react";
+import { JsxElement } from "typescript";
 
 type Props = {
-  label: string;
+  label: string | React.ReactElement;
   onClick?: () => void;
   disabled?: boolean;
   variant?: "full" | "normal";
   className?: string;
+  title?: string;
 };
 
 const Button = (props: Props) => {
-  const { label, disabled, variant = "normal", className, onClick } = props;
+  const {
+    label,
+    disabled,
+    variant = "normal",
+    className,
+    onClick,
+    title,
+  } = props;
   const styles = useMemo(() => ({ full: "w-full", normal: "" }), [variant]);
 
   return (
@@ -17,6 +26,7 @@ const Button = (props: Props) => {
       className={`${className} ${styles[variant]} linear flex items-center justify-center rounded-xl bg-brand-500 px-2 py-2 text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200`}
       disabled={disabled}
       onClick={onClick}
+      title={title}
     >
       {label}
     </button>
