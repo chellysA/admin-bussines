f32578f94ab8a9fc6e8c39dd32859a9f3e364d11
Chellys Castillo <chellys.castillo@gmail.com>
2024-03-06 12:14:36 -0400

    feat: use form added to products

--- app/(dashboard)/productos/components/productsForm.tsx
+++ app/(dashboard)/productos/components/productsForm.tsx
@@ -1,7 +1,11 @@
 import Button from "@/components/button";
-import Checkbox from "@/components/checkbox/CheckboxWithLabel";
-import InputField from "@/components/fields/InputField";
-import Select from "@/components/select";
+import CheckboxController from "@/components/checkbox/CheckboxController";
+import InputController from "@/components/fields/InputController";
+import SelectController from "@/components/select/SelectController";
+import ProductsSchema from "@/data/validations/Products-schema";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 
 type Props = {
   isReadOnly?: boolean;
@@ -10,43 +14,135 @@ type Props = {
 };
 
 const ProductsForm = (props: Props) => {
+  const form = useForm({
+    defaultValues: {
+      nombre: "",
+      categoria: "",
+      sede: "",
+      presentacion: "",
+      precio: "",
+      almacen: "",
+      stock: "",
+    },
+    resolver: yupResolver(ProductsSchema),
+  });
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = form;
+  const router = useRouter();
+
   const { isReadOnly = false, buttonLabel = "", buttonTitle } = props;
+
+  const onSubmit = async (data: any) => {
+    try {
+      await ProductsSchema.validate(data);
+      console.log("Datos válidos:", data);
+      router.push("/productos");
+    } catch (error) {
+      console.error("Error de validación:");
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-8">
-        <InputField label="Nombre" disabled={isReadOnly && true} />
-        <InputField label="Categoria" disabled={isReadOnly && true} />
+        <InputController
+          id="nombre"
+          label="Nombre"
+          disabled={isReadOnly && true}
+          control={control}
+          error={errors.nombre?.message}
+          isError={!!errors.nombre}
+        />
+        <div>
+          <p className="mb-3 ml-3 text-sm text-navy-700 dark:text-white font-bold">
+            Categoria
+          </p>
+          <SelectController
+            id="categoria"
+            options={["Alimentos", "hogar", "Higiene"]}
+            label="Categoria"
+            disabled={isReadOnly && true}
+            control={control}
+            error={errors.categoria?.message}
+            isError={!!errors.categoria}
+          />
+        </div>
         <div>
           <p className="mb-3 ml-3 text-sm text-navy-700 dark:text-white font-bold">
             Sede
           </p>
-          <Select label="Sede 1" options={[]} disabled={isReadOnly && true} />
+          <SelectController
+            id="sede"
+            label="Sede 1"
+            options={["1", "2"]}
+            disabled={isReadOnly && true}
+            control={control}
+            error={errors.sede?.message}
+            isError={!!errors.sede}
+          />
         </div>
         <div>
           <p className="mb-3 ml-3 text-sm text-navy-700 dark:text-white font-bold">
             Presentación
           </p>
-          <Select
+          <SelectController
+            id="presentacion"
             label="Kg"
-            options={["Unidad"]}
+            options={["Unidad", "Docena"]}
             disabled={isReadOnly && true}
+            control={control}
+            isError={!!errors.presentacion}
+            error={errors.presentacion?.message}
           />
         </div>
-        <InputField label="Precio" disabled={isReadOnly && true} />
-        <Checkbox
+        <InputController
+          id="precio"
+          label="Precio"
+          disabled={isReadOnly && true}
+          control={control}
+          isError={!!errors.precio}
+          error={errors.precio?.message}
+        />
+        {/* <CheckboxController
+          id="iva"
           label="Incluye Iva?"
           checkboxes={[
-            { id: "check1", optionsLabel: "Si" },
-            { id: "check2", optionsLabel: "No" },
+            { id: "1check", optionsLabel: "Si", name: "check1" },
+            { id: "check2", optionsLabel: "No", name: "check2" },
           ]}
           disabled={isReadOnly}
+          control={control}
+          isError={!!errors.iva}
+          error={errors.iva?.message}
+        /> */}
+        <InputController
+          id="almacen"
+          label="Cantidad en almacén"
+          disabled={isReadOnly && true}
+          control={control}
+          isError={!!errors.almacen}
+          error={errors.almacen?.message}
         />
-        <InputField label="Cantidad en almacén" disabled={isReadOnly && true} />
-        <InputField label="Stock" disabled={isReadOnly && true} />
+        <InputController
+          id="stock"
+          label="Stock"
+          disabled={isReadOnly && true}
+          control={control}
+          isError={!!errors.stock}
+          error={errors.stock?.message}
+        />
       </div>
       {!isReadOnly && (
         <div className="flex justify-end mt-8">
-          <Button label={buttonLabel} className="px-8" title={buttonTitle} />
+          <Button
+            label={buttonLabel}
+            className="px-8"
+            title={buttonTitle}
+            type="submit"
+          />
         </div>
       )}
     </form>
--- app/(dashboard)/usuarios/page.tsx
+++ app/(dashboard)/usuarios/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Button from "@/components/button";
 import InputField from "@/components/fields/InputField";
 import BasicTable from "../../../components/tables/basicTable";
@@ -10,16 +10,17 @@ import Link from "next/link";
 import useChangeTitleLayoutAdmin from "@/hooks/useChangeTiTleLayout";
 
 const Usuarios = () => {
-  useChangeTitleLayoutAdmin("Usuarios")
+  useChangeTitleLayoutAdmin("Usuarios");
   return (
     <>
       <div className="mt-3 grid grid-cols-1 md:gap-5 md:grid-cols-3">
         <div className="grid grid-cols-1 grid-rows-1 col-span-2 gap-5 md:grid-cols-3 md:mr-10 mt-2">
-          <InputField placeholder="Nombre" />
-          <InputField placeholder="Email" />
+          <InputField placeholder="Nombre" id="nombre" />
+          <InputField placeholder="Email" id="email" />
           <Select
             options={["Administrador", "Gerente", "Cajero", "Vendedor"]}
             label="Cargo"
+            id="cargo"
           />
         </div>
         <div className="md:ml-12 mt-6 md:mt-2 grid grid-cols-1 col-span-1">
