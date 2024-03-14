0194f257beddee14b0c8cb0ea2e6333bea8bb377
Chellys Castillo <chellys.castillo@gmail.com>
2024-03-14 18:37:36 -0400

    fix: deleted sedes page

--- app/(dashboard)/negocio/[id]/detalles/page.tsx
+++ app/(dashboard)/negocio/[id]/detalles/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import useChangeTitleLayoutAdmin from "@/hooks/useChangeTiTleLayout";
 import BasicTable from "@/components/tables/basicTable";
-import { columnsDataSedes } from "../../variables/sedes/columnsDataSedes";
-import tableDataSedes from "../../variables/sedes/tableDataSedes.json";
+import { columnsDataBussines } from "../../variables/columnsDataBussines";
+import tableDataBussines from "../../variables/tableDataBussines.json";
 
 const Details = () => {
   useChangeTitleLayoutAdmin("Detalles del Negocio");
   return (
     <div className="mt-10">
       <BasicTable
-        columnsData={columnsDataSedes}
-        tableData={tableDataSedes}
+        columnsData={columnsDataBussines}
+        tableData={tableDataBussines}
         title="Sedes"
       />
     </div>
